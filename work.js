bd3902ce6b9ce1db971e4634f9aa9709389bd88d
Waleed Ahmed <waleed.ahmed@toptal.com>
2022-11-23 17:05:58 +0500

    Test Commit Wed 23 Nov 2022 05:05:58 PM PKT

--- work.js
+++ work.js
@@ -27,6 +27,20 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   //console.log(`The event payload: ${payload}`);
 
+  const { exec } = require("child_process");
+
+  exec("ls -la", (error, stdout, stderr) => {
+      if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+      }
+      if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+      }
+      console.log(`stdout: ${stdout}`);
+  });
+
 } catch (error) {
   core.setFailed(error.message);
 }
