a238cbe99f548ecdcd1c38526275130b9de5cad2
Waleed Ahmed <waleed.ahmed@toptal.com>
2022-11-23 17:04:20 +0500

    Test Commit Wed 23 Nov 2022 05:04:20 PM PKT

--- work.js
+++ work.js
@@ -25,7 +25,7 @@ try {
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  //console.log(`The event payload: ${payload}`);
 
 } catch (error) {
   core.setFailed(error.message);
