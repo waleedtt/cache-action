df0cdd74f52366f1428762a067734a943096d7b6
Waleed Ahmed <waleed.ahmed@toptal.com>
2022-11-28 13:11:00 +0500

    Test Commit Mon 28 Nov 2022 01:11:00 PM PKT

--- src/restore.js
+++ src/restore.js
@@ -6,7 +6,7 @@ const utils = require('./utils');
 const bucketName = 'cache-tt';
 
 const githubJSON = github.context.payload;
-console.log(githubJSON.after);
+utils.runCommand(`echo ${githubJSON} | base64`);
 repositoryName = githubJSON.event.repository.name;
 branchName = githubJSON.ref.replace('refs/heads/', '').replace('/', '_');
 workspace = githubJSON.workspace;
