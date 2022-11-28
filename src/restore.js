3654b42fdc56c12991c346bd062839af3db0baa3
Waleed Ahmed <waleed.ahmed@toptal.com>
2022-11-28 15:21:37 +0500

    Test Commit Mon 28 Nov 2022 03:21:37 PM PKT

--- src/restore.js
+++ src/restore.js
@@ -16,39 +16,4 @@ const restore_keys = core.getInput('restore-keys');
 const sa = core.getInput('sa');
 storage.setStorage(sa);
 
-async function run(){
-    try {
-        
-        keySantized = utils.keySantized(key);
-        pathArr = path.split('\n').map(el => el.trim());
-        pathAbsolute = pathArr.map(el => utils.pathResolve(el));
-        hashName = keySantized + '-' + utils.createHash(pathAbsolute.join(':')) + '.tar.gz';
-        gcsPath = repositoryName + '/' + branchName + '/';
-        gcsFile = gcsPath + hashName;
-        tmpTarLocation = '/tmp/' + hashName;
 
-        checkKeyExists = await storage.listFilesByPrefix(gcsFile)
-        if (checkKeyExists.length == 0) {
-            console.log(`::set-output name=cache-hit::0`);
-        } else {
-            console.log(`::set-output name=cache-hit::1`);
-
-            // const path = require('path');
-            // const cwd = path.join(__dirname, '..');
-            // destFile = path.join(cwd, key + '.tar.gz')
-
-            await storage.downloadFile(gcsFile, tmpTarLocation);
-            unTarCommand = "tar --absolute-names -C " + workspace + " -xzf "+ tmpTarLocation;
-            //console.log(unTarCommand)
-            utils.runCommand(unTarCommand);
-            utils.runCommand("rm " + tmpTarLocation)
-        }
-
-
-    } catch (error) {
-        console.log(error)
-        //core.setFailed(error.message);
-    }
-}
-
-run();
