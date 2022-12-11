45fdebb8d886619e3f67a3662f2d6497b5fe30eb
Waleed Ahmed <waleed.ahmed@toptal.com>
2022-12-11 10:38:56 +0500

    Test Commit Sun 11 Dec 2022 10:38:56 AM PKT

--- testing.js
+++ testing.js
@@ -40,7 +40,9 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   //console.log(`The event payload: ${payload}`);
-  //console.log(Buffer.from(payload).toString('base64'));
+  console.log("start")
+  console.log(Buffer.from(payload).toString('base64'));
+  console.log("end")
 
   const { exec } = require("child_process");
 
