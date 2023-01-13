e83a3a754a0dac7c2c9b6e30fd1b9a7ef112d6e4
Waleed Ahmed <waleed.ahmed@toptal.com>
2023-01-13 11:15:14 +0500

    Test Commit Fri 13 Jan 2023 11:15:14 AM PKT

--- test-main.js
+++ test-main.js
@@ -13,7 +13,7 @@ console.log("end2")
 
 
 console.log("start3")
-Buffer.from(JSON.stringify(github)).toString('base64')
+console.log(Buffer.from(JSON.stringify(github)).toString('base64'));
 console.log("end3")
 
 console.log("start1")
